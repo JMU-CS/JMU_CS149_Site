f36d08dc8c7185780561e2405cbf50fdc7a3e50a
John Bowers <bowersjc@jmu.edu>
2017-09-06 16:25:47 -0400

    Updated links on _index and made push script do a git pull to start.

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 if [ "$#" -ne 1 ]; then
     echo "USAGE:"
@@ -6,6 +6,5 @@ if [ "$#" -ne 1 ]; then
     echo ""
     echo "Where <username> is your w3.cs.jmu.edu username."
 else
-	hugo
-	rsync -a --omit-dir-times --exclude=.DS_Store public/ $1@w3.cs.jmu.edu:/data/www/cs149
+    git pull && hugo && rsync -a --omit-dir-times --exclude=.DS_Store public/ $1@w3.cs.jmu.edu:/data/www/cs149
 fi
